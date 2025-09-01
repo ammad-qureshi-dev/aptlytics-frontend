7d336cea41bf38a650376db420050a814e52b5f7
ammad-qureshi-dev <ammadq87@my.yorku.ca>
2025-09-01 19:36:51 -0400

    feat: added hamburger menu on smaller screens

--- src/components/navigation/AccountNavbar.tsx
+++ src/components/navigation/AccountNavbar.tsx
@@ -1,7 +1,14 @@
+'use effect';
+
+import { Menu, X } from "lucide-react";
 import NavItem from "./NavItem";
 import { NavItemType } from "./Types";
+import HamburgerNavbar from "./HamburgerNavbar";
+import { useState } from "react";
 
 export default function AccountNavbar() {
+
+    const [showHamburgerMenu, setShowHamburgerMenu] = useState<boolean>();
     const accountNavItems: NavItemType[] = [
         {
             href: "/notifications",
@@ -16,7 +23,7 @@ export default function AccountNavbar() {
     ];
 
     return (
-        <div>
+        <div className="flex flex-row items-center gap-4 mx-4">
             <ul id="account-navbar" className="flex flex-row gap-8 w-fit px-4">
                 {
                     accountNavItems.map((item, key) => {
@@ -28,7 +35,22 @@ export default function AccountNavbar() {
                     })
                 }
             </ul>
+            <div className="flex items-center lg:hidden">
+                <button type="button" onClick={() => { setShowHamburgerMenu(!showHamburgerMenu) }} className="cursor-pointer">
+                    {
+                        showHamburgerMenu &&
+                        <X />
+                    }
+                    {
+                        !showHamburgerMenu &&
+                        <Menu />
+                    }
+                    {
+                        showHamburgerMenu &&
+                        <HamburgerNavbar />
+                    }
+                </button>
+            </div>
         </div>
-
     )
 }
